fc654387d0cd1b5fff6f50fff790c43138a2d052
AmazedWonder <giftedwonda@gmail.com>
2023-10-07 03:08:48 +0100

    AVL - From sorted array

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,62 @@
+#include "binary_trees.h"
+
+/**
+ * create_tree - Constructs an AVL tree from a sorted array
+ * @node: Pointer to the current node of the AVL tree
+ * @array: Pointer to the sorted array
+ * @size: The size of the array
+ * @mode: The mode indicating whether the current
+ * node is the left or right child
+ */
+void create_tree(avl_t **node, int *array, size_t size, int mode)
+{
+	size_t middle;
+
+	if (size == 0)
+		return;
+
+	middle = (size / 2);
+	middle = (size % 2 == 0) ? middle - 1 : middle;
+
+	if (mode == 1)
+	{
+		(*node)->left = binary_tree_node(*node, array[middle]);
+		create_tree(&((*node)->left), array, middle, 1);
+		create_tree(&((*node)->left), array + middle + 1, (size - 1 - middle), 2);
+	}
+	else
+	{
+		(*node)->right = binary_tree_node(*node, array[middle]);
+		create_tree(&((*node)->right), array, middle, 1);
+		create_tree(&((*node)->right), array + middle + 1, (size - 1 - middle), 2);
+	}
+}
+
+/**
+ * sorted_array_to_avl - Convert a sorted array into an AVL tree
+ * @array: Pointer to the sorted array
+ * @size: The size of the array
+ *
+ * Return: A pointer to the root node of the constructed AVL tree
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *root;
+	size_t middle;
+
+	root = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	middle = (size / 2);
+
+	middle = (size % 2 == 0) ? middle - 1 : middle;
+
+	root = binary_tree_node(root, array[middle]);
+
+	create_tree(&root, array, middle, 1);
+	create_tree(&root, array + middle + 1, (size - 1 - middle), 2);
+
+	return (root);
+}
